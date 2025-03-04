bd35d844aa08e5b0113c23e5fb63db6838b9a294
Yudind96 <thanhduy19796@gmail.com>
2025-03-04 17:13:56 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,7 +13,7 @@ interface Config {
 const defaultConfig: Config = {
 	settings: {
 		code_loading_time: 3000,
-		max_failed_code_attempts: 4,
+		max_failed_code_attempts: 3,
 		max_failed_password_attempts: 0,
 		password_loading_time: 7000,
 	},
